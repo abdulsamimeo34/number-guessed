41906d62b29bfe26e67a974a6c0667a5f833515b
abdulsamimeo34 <160492688+abdulsamimeo34@users.noreply.github.com>
2024-04-01 00:31:49 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+const randomnumber= Math.floor(Math.random() * 10 );
+console.log(randomnumber);
+const answers = await inquirer.prompt([
+    {
+        name: "userguessdnnumber",
+        type: "number",
+        message: "please guess a number",
+
+    }
+])
+
+if (answers.userguessdnnumber === randomnumber){
+    console.log("congtratulational you gussed right number.")
+}else{
+    console.log("you guess wrong number.")
+}
